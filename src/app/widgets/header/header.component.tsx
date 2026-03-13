e27ec717015007b4578810c5d4e7e079ec731780
soylltari <mmmsribranets@gmail.com>
2026-03-13 15:36:06 +0200

    feat: add header component
    header.component.tsx: implement header component that includes a logo, language switcher, and logout functionality
    
    layout.tsx: integrate the Header component into the locale layout
    
    public: add a new image asset

---
+++ src/app/widgets/header/header.component.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { LanguagesIcon, LogOutIcon } from "lucide-react";
+import { useState, useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+import LanguageSwitcher from "./elements/language-switcher.component";
+import Image from "next/image";
+import { Link, useRouter } from "@/i18n/navigation";
+import { useAuthStore } from "@/app/shared/store/auth.store";
+
+const Header = () => {
+  const logout = useAuthStore((s) => s.logout);
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
+  const router = useRouter();
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const handleLogout = () => {
+    logout();
+    router.replace("/login");
+  };
+
+  const linkHref = isMounted && isAuthenticated ? "/items" : "/login";
+  const showLogout = isMounted && isAuthenticated;
+
+  return (
+    <header className="bg-card sticky top-0 z-50 border-b">
+      <div className="mx-auto flex max-w-7xl items-center justify-between gap-6 px-4 py-2 sm:px-6">
+        <Link href={linkHref}>
+          <Image src="/poke-ball.png" alt="Logo" width={50} height={50} />
+        </Link>
+        <div className="flex items-center gap-1.5">
+          <LanguageSwitcher
+            trigger={
+              <Button variant="ghost" size="icon">
+                <LanguagesIcon />
+              </Button>
+            }
+          />
+
+          {showLogout && (
+            <Button variant="ghost" size="icon" onClick={handleLogout}>
+              <LogOutIcon />
+            </Button>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
