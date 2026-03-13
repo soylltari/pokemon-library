7810df38690b040904362bf3e534b09ecb9974b1
soylltari <mmmsribranets@gmail.com>
2026-03-13 16:07:25 +0200

    fix: enhance localization
    layout.tsx: add generateStaticParams
    
    page.tsx: remove hardcoded redirect

--- src/app/(web)/[locale]/layout.tsx
+++ src/app/(web)/[locale]/layout.tsx
@@ -1,6 +1,11 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
 import Header from "./index";
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export default async function LocaleLayout({
   children,
