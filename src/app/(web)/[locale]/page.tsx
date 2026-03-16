399e4aa1f4e0e52a071772c5747dcdb6aed37769
soylltari <mmmsribranets@gmail.com>
2026-03-16 21:04:48 +0200

    fix: update page.tsx to handle authentication redirection
    (web)/[locale]/page.tsx: check for an auth-token cookie and redirect users based on authentication status.

--- src/app/(web)/[locale]/page.tsx
+++ src/app/(web)/[locale]/page.tsx
@@ -1,6 +1,10 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { routing } from "@/i18n/routing";
 
-export default function RootPage() {
-  redirect(`/${routing.defaultLocale}/login`);
+export default async function RootPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("auth-token")?.value;
+
+  return redirect(`/${routing.defaultLocale}/${token ? "items" : "login"}`);
 }
