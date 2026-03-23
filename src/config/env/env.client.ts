b6e5a2b4798e11ce3d035077511497f9b60fbc10
soylltari <mmmsribranets@gmail.com>
2026-03-23 16:39:13 +0200

    feat: add environment client configuration for API URL

---
+++ src/config/env/env.client.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+import { createEnv } from '@t3-oss/env-nextjs'
+
+// env client
+export const envClient = createEnv({
+  client: {
+    NEXT_PUBLIC_CLIENT_API_URL: z.string().nonempty({ message: 'NEXT_PUBLIC_CLIENT_API_URL is required' }),
+  },
+  emptyStringAsUndefined: true,
+  runtimeEnv: {
+    NEXT_PUBLIC_CLIENT_API_URL: process.env.NEXT_PUBLIC_CLIENT_API_URL,
+  },
+})
